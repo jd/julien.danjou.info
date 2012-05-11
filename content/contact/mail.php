4ed45074433a3af6fd369342ab34efd630a2fcd6
Julien Danjou <julien@danjou.info>
2012-05-11 15:59:07 +0200

    Post to mail.php via Ajax
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- content/contact/mail.php
+++ content/contact/mail.php
@@ -1,24 +1,17 @@
-<div class="hero-unit">
-  <p>
+---
+extends:
+---
 <?
 if($_POST['message'] != "" && $_POST['mail'] != "")
 {
-  mail("julien+contact@danjou.info",
-       "Contact JDI",
-       "Last name: " . $_POST['lastname'] . "\n" .
-       "First name: " . $_POST['firstname'] . "\n" .
-       "Company: " . $_POST['company'] . "\n" .
-       "Phone: " . $_POST['phone'] . "\n" .
-       "Message:\n" . $_POST['message'],
-       "From: " . $_POST['firstname'] . " " . $_POST['lastname'] . " <" . $_POST['mail'] . ">");
-
-  print "Your mail has been sent successfully! I'll get back to you in the next 48 hours.</p>";
-}
-else
-{
-  echo "<span class=\"alert-error\">Your mail hasn't been sent!</span></p>";
-  echo "<a href=\"/contact\"><span class=\"btn btn-warning\">Return to contact form</span></a>";
+  if(mail("julien+contact@danjou.info",
+          "Contact JDI",
+          "Last name: " . $_POST['lastname'] . "\n" .
+          "First name: " . $_POST['firstname'] . "\n" .
+          "Company: " . $_POST['company'] . "\n" .
+          "Phone: " . $_POST['phone'] . "\n" .
+          "Message:\n" . $_POST['message'],
+          "From: " . $_POST['firstname'] . " " . $_POST['lastname'] . " <" . $_POST['mail'] . ">"))
+     { echo "OK"; }
 }
 ?>
-
-</div>
