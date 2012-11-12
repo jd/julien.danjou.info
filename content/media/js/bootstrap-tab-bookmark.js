b1ac1ae37109b428b60279656f1e87ba7740c174
Julien Danjou <julien@danjou.info>
2012-11-12 17:10:48 +0100

    New layout
    Signed-off-by: Julien Danjou <julien@danjou.info>

--- content/media/js/bootstrap-tab-bookmark.js
+++ content/media/js/bootstrap-tab-bookmark.js
@@ -3,13 +3,16 @@ function bootstrap_tab_bookmark (selector) {
         selector = "";
     }
 
-    /* Automagically jump on good tab based on anchor */
-    $(document).ready(function() {
+    var bookmark_switch = function () {
         url = document.location.href.split('#');
         if(url[1] != undefined) {
             $(selector + '[href=#'+url[1]+']').tab('show');
         }
-    });
+    }
+
+    /* Automagically jump on good tab based on anchor */
+    $(document).ready(bookmark_switch);
+    $(window).bind('hashchange', bookmark_switch);
 
     var update_location = function (event) {
         document.location.hash = this.getAttribute("href");
